fbca2b50dccf60baba2a56746ecdb840f46d40bf
sarojkumarmohanty <sarojkumarmohanty@gmail.com>
2023-10-06 11:39:50 +0530

    and one more line

--- src/main/java/tech/csm/controller/Runner.java
+++ src/main/java/tech/csm/controller/Runner.java
@@ -5,6 +5,7 @@ public class Runner {
 	public static void main(String[] args) {
 		System.out.println("working... with maven");
 		System.out.println("working fine...");
+		System.out.println("and one more line");
 	}
 
 }
